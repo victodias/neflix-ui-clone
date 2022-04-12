6ff5602b2b138d5927097230f23642d1f7e38c96
Camila Zeron <camilazeron10@gmail.com>
2022-04-12 14:27:58 -0300

    update

--- src/components/MovieRow/index.js
+++ src/components/MovieRow/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import './components/MovieRow/index.css';
-// import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
-// import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 
 export default ({title, items}) => {
  return(
     <div className="movieRow">
         <h2>{title}</h2>
         <div className="movieRow--left">
-            {/* <NavigateBeforeIcon style={{fontSize:50}}/> */}
+            <NavigateBeforeIcon style={{fontSize:50}}/>
         </div>
         <div className="movieRow--right">
-            {/* <NavigateNextIcon style={{fontSize:50}}/> */}
+            <NavigateNextIcon style={{fontSize:50}}/>
         </div>
         <div className="movieRow--listarea">
         <div className="movieRow--list"> 
