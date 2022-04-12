ada2e790f425d8a6dfff74fc5f2cdb38af8d4d9e
victordias <victoroliveirad96@gmail.com>
2022-04-12 19:23:34 -0300

    feature(export-named): adiciona um nome à exportacao do componente funcional <App />

--- src/App.js
+++ src/App.js
@@ -5,14 +5,12 @@ import MovieRow from './components/MovieRow/index.js';
 import FeaturedMovie from './components/FeaturedMovie/index.js';
 import Header from './components/Header/index.js';
 
+export const App = () => {
+    const [movieList, setMovieList] = useState([]);
+    const [featureData, setFeaturedData] = useState([]);
+    const [blackHeader, setBlackHeader] = useState(false);
 
-export default () => {
-
-const[movieList, setMovieList] = useState([]);
-const[featureData, setFeaturedData] = useState([]);
-const[blackHeader, setBlackHeader] = useState(false);
-
-    useEffect(() =>{
+    useEffect(() => {
         const loadAll = async () => {
             //pegando lista TOTAL 
             let list = await Tmdb.getHomeList();
@@ -20,47 +18,48 @@ const[blackHeader, setBlackHeader] = useState(false);
 
             //pegando filme em DESTAQUE
             let originals = list.filter(i => i.slug === 'originals');
-            let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length -1));
+            let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
             let chosen = originals[0].items.results[randomChosen];
             let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
             setFeaturedData(chosenInfo);
 
-           
 
-          
+
+
         }
         loadAll();
     }, []);
 
 
-useEffect(() => {
-    const scrollListener = () => {
-        if(window.scrollY > 10){
-            setBlackHeader(true);
-        } else{
-            setBlackHeader(false);
+    useEffect(() => {
+        const scrollListener = () => {
+            if (window.scrollY > 10) {
+                setBlackHeader(true);
+            } else {
+                setBlackHeader(false);
+            }
         }
-    }
-    window.addEventListener('scroll', scrollListener);
+        window.addEventListener('scroll', scrollListener);
 
-    return() => {
-        window.removeEventListener('scroll', scrollListener);
-    }
-}, []);
+        return () => {
+            window.removeEventListener('scroll', scrollListener);
+        }
+    }, []);
 
-    return(
+    return (
         <div className="page">
 
-            <Header black={blackHeader}/>
+            <Header black={blackHeader} />
 
-            {featureData && 
-               <FeaturedMovie item={featureData}/>
+            {
+                featureData &&
+                <FeaturedMovie item={featureData} />
             }
-           <section className="lists">{movieList.map((item, key)=> (
-               <MovieRow key={key} title={item.title} items={item.items}/>
-           ))}
-           </section> 
-           <footer> Feito com <span role="img" aria-label="coração">♥ por Camila Zeron</span><br/>Direitos de imagem para Netflix<br/>Dados utilizados por Themoviedb.org</footer>
-        </div>
+            <section className="lists">{movieList.map((item, key) => (
+                <MovieRow key={key} title={item.title} items={item.items} />
+            ))}
+            </section>
+            <footer> Feito com <span role="img" aria-label="coração">♥ por Camila Zeron</span><br />Direitos de imagem para Netflix<br />Dados utilizados por Themoviedb.org</footer>
+        </div >
     );
 }
